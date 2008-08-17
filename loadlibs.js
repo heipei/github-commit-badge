b6189878e8a5e67227291764eb6757e8a80952d1
Johannes Gilger <heipei@hackvalue.de>
2008-08-17 18:08:20 +0200

    Working version for Safari and FF3

--- loadlibs.js
+++ loadlibs.js
@@ -1,6 +1,15 @@
-['md5', 'jquery', 'date'].each(function(lib) {
-	var myLib = document.createElement("script");
-	myLib.setAttribute("type","text/javascript");
-	myLib.setAttribute("src","lib/" + lib + ".js");
-	document.documentElement.appendChild(myLib);
-});
+// Load the necessary libraries
+var myLibs = ["jquery", "md5", "date", "prototype"];
+for (var i=0; i < myLibs.length; ++i) {
+	var myScript = document.createElement("script");
+	myScript.setAttribute("type","text/javascript");
+	myScript.setAttribute("src","lib/" + myLibs[i] + ".js");
+	document.documentElement.appendChild(myScript);
+};
+// Write the stylesheet into the <head>
+myHead = document.getElementsByTagName("head")[0];
+myCSS = document.createElement("link");
+myCSS.setAttribute("rel","stylesheet");
+myCSS.setAttribute("type","text/css");
+myCSS.setAttribute("href","style.css");
+myHead.appendChild(myCSS);
--- test.js
+++ test.js
@@ -2,44 +2,49 @@
 // by heipei
 
 
-var myBadge = document.createElement("div");
-myBadge.setAttribute("class","outline");
+Badges.each(function(badgeData) {
 
-var myUserRepo = document.createElement("div");
-myUserRepo.setAttribute("class","username");
+jQuery.getJSON("http://github.com/api/v1/json/" + badgeData["username"] + "/" + badgeData["repo"] 
+	+ "/commit/" + badgeData["branch"] + "?callback=?", function(data) {
+		
+		var myUser = badgeData["username"];
+		var myRepo = badgeData["repo"];
+		var myEval = eval ( data );
+		
+		var myBadge = document.createElement("div");
+		myBadge.setAttribute("class","outline");
 
-var myLink = document.createElement("a");
-myLink.setAttribute("href","http://github.com/" + USERNAME + "/" + REPO);
-myLink.setAttribute("class","username");
-myLink.appendChild(document.createTextNode(USERNAME + "/" + REPO));
-myUserRepo.appendChild(myLink);
+		var myUserRepo = document.createElement("div");
+		myUserRepo.setAttribute("class","username");
 
-var myCommitMessage = document.createElement("div");
-myCommitMessage.setAttribute("class","commitmessage");
-var myDiffLine = document.createElement("div");
-myDiffLine.setAttribute("class","diffline");
+		var myLink = document.createElement("a");
+		myLink.setAttribute("href","http://github.com/" + myUser + "/" + myRepo);
+		myLink.setAttribute("class","username");
+		myLink.appendChild(document.createTextNode(myUser + "/" + myRepo));
+		myUserRepo.appendChild(myLink);
 
-myDiffStat = document.createElement("div");
-myDiffStat.setAttribute("class","diffstat");
+		var myCommitMessage = document.createElement("div");
+		myCommitMessage.setAttribute("class","commitmessage");
+		var myDiffLine = document.createElement("div");
+		myDiffLine.setAttribute("class","diffline");
 
-jQuery.getJSON("http://github.com/api/v1/json/" + USERNAME + "/" + REPO + "/commit/" + BRANCH + "?callback=?",
-	function(data) {
-		MyEval = eval ( data );
+		var myDiffStat = document.createElement("div");
+		myDiffStat.setAttribute("class","diffstat");
 		
 		var myImage = document.createElement("img");
-		myImage.setAttribute("src","http://www.gravatar.com/avatar/" + hex_md5(MyEval.commit.committer.email) + "?s=60");
+		myImage.setAttribute("src","http://www.gravatar.com/avatar/" + hex_md5(myEval.commit.committer.email) + "?s=60");
 		myImage.className = "gravatar";
 		myDiffLine.appendChild(myImage);
 		
 		var myLink = document.createElement("a");
-		myLink.setAttribute("href",MyEval.commit.url);
-		myLink.appendChild(document.createTextNode(" " + MyEval.commit.id.truncate(10,"")));
-		myDiffLine.appendChild(document.createTextNode(MyEval.commit.committer.name + " "));
-		mySpan = document.createElement("span");
+		myLink.setAttribute("href",myEval.commit.url);
+		myLink.appendChild(document.createTextNode(" " + myEval.commit.id.truncate(10,"")));
+		myDiffLine.appendChild(document.createTextNode(myEval.commit.committer.name + " "));
+		var mySpan = document.createElement("span");
 		mySpan.appendChild(document.createTextNode("committed"));
 		
 		var myDate = document.createElement("span");
-		var myParsedDate = Date.parse(MyEval.commit.committed_date).toString("yyyy-MM-dd @ HH:mm");
+		var myParsedDate = Date.parse(myEval.commit.committed_date).toString("yyyy-MM-dd @ HH:mm");
 		myDate.appendChild(document.createTextNode(myParsedDate));
 		
 		myDiffLine.appendChild(mySpan);
@@ -47,29 +52,15 @@ jQuery.getJSON("http://github.com/api/v1/json/" + USERNAME + "/" + REPO + "/comm
 		myDiffLine.appendChild(document.createElement("span").appendChild(document.createTextNode(" on ")));
 		myDiffLine.appendChild(myDate);
 		
-		myCommitMessage.appendChild(document.createTextNode("\"" + MyEval.commit.message + "\""));
-		/* myAdded = document.createElement("span");
-		myAdded.id = "diffadded";
-		myAdded.appendChild(document.createTextNode(" added"));
-		myRemoved = document.createElement("span");
-		myRemoved.appendChild(document.createTextNode(" removed"));
-		myRemoved.id = "diffremoved";
-		myChanged = document.createElement("span");
-		myChanged.appendChild(document.createTextNode(" changed"));
-		myChanged.id = "diffchanged";
-		myDiffStat.appendChild(document.createTextNode(" (" + MyEval.commit.added.length));
-		myDiffStat.appendChild(myAdded);
-		myDiffStat.appendChild(document.createTextNode(", " + MyEval.commit.removed.length));
-		myDiffStat.appendChild(myRemoved);
-		myDiffStat.appendChild(document.createTextNode(", " + MyEval.commit.modified.length));
-		myDiffStat.appendChild(myChanged);
-		myDiffStat.appendChild(document.createTextNode(")")); */
-		myDiffStat.innerHTML = "( " + MyEval.commit.added.length + " <span id=\"diffadded\">added<\/span>, " + MyEval.commit.removed.length + " <span id=\"diffremoved\">removed<\/span>, " + MyEval.commit.modified.length + " <span id=\"diffchanged\">changed<\/span>)";
-	});
-
-myBadge.appendChild(myUserRepo);
-myBadge.appendChild(myDiffLine);
-myBadge.appendChild(myCommitMessage);
-myBadge.appendChild(myDiffStat);
-myContainer = document.getElementById("container");
-myContainer.appendChild(myBadge);
+		myCommitMessage.appendChild(document.createTextNode("\"" + myEval.commit.message + "\""));
+		myDiffStat.innerHTML = "(" + myEval.commit.added.length + " <span id=\"diffadded\">added<\/span>, " 
+			+ myEval.commit.removed.length + " <span id=\"diffremoved\">removed<\/span>, " 
+			+ myEval.commit.modified.length + " <span id=\"diffchanged\">changed<\/span>)";
+		myBadge.appendChild(myUserRepo);
+		myBadge.appendChild(myDiffLine);
+		myBadge.appendChild(myCommitMessage);
+		myBadge.appendChild(myDiffStat);
+		var myContainer = document.getElementById("container");
+		myContainer.appendChild(myBadge);
+});
+});
