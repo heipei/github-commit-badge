4b766014983ed3bc1435feac1fddf394a0fc6e27
NumEricR <->
2011-06-10 19:29:38 +0200

    Simplification of CSS classnames

--- github-commit-badge.js
+++ github-commit-badge.js
@@ -40,38 +40,37 @@ jQuery.getJSON("http://github.com/api/v1/json/" + badgeData["username"] + "/" + 
 		
 		// outline-class is used for the badge with the border
 		var myBadge = document.createElement("div");
-		myBadge.setAttribute("class","github-commit-badge-outline");
+		myBadge.setAttribute("class","outline");
 
 		// the username/repo
 		var myUserRepo = document.createElement("div");
-		myUserRepo.setAttribute("class","github-commit-badge-username");
+		myUserRepo.setAttribute("class","username");
 
 		var myLink = document.createElement("a");
 		myLink.setAttribute("href","http://github.com/" + myUser + "/" + myRepo);
-		myLink.setAttribute("class","github-commit-badge-username");
 		myLink.appendChild(document.createTextNode(myUser + "/" + myRepo));
 		myUserRepo.appendChild(myLink);
 
 		// myDiffLine is the "foo committed xy on date" line 
 		var myDiffLine = document.createElement("div");
-		myDiffLine.setAttribute("class", "github-commit-badge-diffline");
+		myDiffLine.setAttribute("class", "diffline");
 	
 		// the image-class uses float:left to sit left of the commit-message
 		var myImage = document.createElement("img");
 		myImage.setAttribute("src","http://www.gravatar.com/avatar/" + hex_md5(myEval.commit.committer.email) + "?s=60");
-		myImage.setAttribute("class","github-commit-badge-gravatar");
+		myImage.setAttribute("class","gravatar");
 		myImage.setAttribute("alt",myUser + myRepo);
 		myDiffLine.appendChild(myImage);
 		
 		var myLink = document.createElement("a");
 		myLink.setAttribute("href",myEval.commit.url);
-		myLink.setAttribute("class", "github-commit-badge-badge");
+		myLink.setAttribute("class", "badge");
 		myLink.appendChild(document.createTextNode(" " + truncate(myEval.commit.id,10,"")));
 		myDiffLine.appendChild(document.createTextNode(myEval.commit.committer.name + " committed "));
 		
 		var myDate = document.createElement("span");
 		var dateTime = parseDate(myEval.commit.committed_date);
-		myDate.setAttribute("class", "github-commit-badge-text-date");
+		myDate.setAttribute("class", "text-date");
 		myDate.setAttribute("title", dateTime);
 		myDate.appendChild(document.createTextNode(dateTime));
 		
@@ -81,28 +80,28 @@ jQuery.getJSON("http://github.com/api/v1/json/" + badgeData["username"] + "/" + 
 		
 		// myCommitMessage is the commit-message
 		var myCommitMessage = document.createElement("div");
-		myCommitMessage.setAttribute("class", "github-commit-badge-commitmessage");
+		myCommitMessage.setAttribute("class", "commitmessage");
 		myCommitMessage.appendChild(document.createTextNode("\"" + truncate(myEval.commit.message,100) + "\""));
 		
 		// myDiffStat shows how many files were added/removed/changed
 		var myDiffStat = document.createElement("div");
-		myDiffStat.setAttribute("class", "github-commit-badge-diffstat");
-		myDiffStat.innerHTML = "(" + added.length + " <span class=\"github-commit-badge-diffadded\">added<\/span>, " 
-			+ removed.length + " <span class=\"github-commit-badge-diffremoved\">removed<\/span>, " 
-			+ modified.length + " <span class=\"github-commit-badge-diffchanged\">changed<\/span>) ";
+		myDiffStat.setAttribute("class", "diffstat");
+		myDiffStat.innerHTML = "(" + added.length + " <span class=\"diffadded\">added<\/span>, " 
+			+ removed.length + " <span class=\"diffremoved\">removed<\/span>, " 
+			+ modified.length + " <span class=\"diffchanged\">changed<\/span>) ";
 		
 		// only show the "Show files" button if the commit actually added/removed/modified any files at all
 		if (added.length != "0" || removed.length != "0" || modified.length != "0") {
-			myDiffStat.innerHTML = myDiffStat.innerHTML + "<a href=\"\" class=\"github-commit-badge-showMoreLink\" id=\"showMoreLink" + myUser + myRepo + "\">Show files<\/a>";
+			myDiffStat.innerHTML = myDiffStat.innerHTML + "<a href=\"\" class=\"showMoreLink\" id=\"showMoreLink" + myUser + myRepo + "\">Show files<\/a>";
 		};
 
 		// myFileList lists addded/remove/changed files, hidden at startup
 		var myFileList = document.createElement("div");
-		myFileList.setAttribute("class", "github-commit-badge-filelist");
+		myFileList.setAttribute("class", "filelist");
 		myFileList.setAttribute("id", myUser + myRepo);
 
 		var myAddedFileList = document.createElement("div");
-		myAddedFileList.innerHTML = "<span class=\"github-commit-badge-diffadded\">Added:<\/span>";
+		myAddedFileList.innerHTML = "<span class=\"diffadded\">Added:<\/span>";
 		var myList = document.createElement("ul");
 		jQuery.each(added, function(j, myAdded) {
 			var myFile = document.createElement("li");
@@ -112,7 +111,7 @@ jQuery.getJSON("http://github.com/api/v1/json/" + badgeData["username"] + "/" + 
 		myAddedFileList.appendChild(myList);
 		
 		var myRemovedFileList = document.createElement("div");
-		myRemovedFileList.innerHTML = "<span class=\"github-commit-badge-diffremoved\">Removed:<\/span>";
+		myRemovedFileList.innerHTML = "<span class=\"diffremoved\">Removed:<\/span>";
 		var myList = document.createElement("ul");
 		jQuery.each(removed, function(j, myRemoved) {
 			var myFile = document.createElement("li");
@@ -122,7 +121,7 @@ jQuery.getJSON("http://github.com/api/v1/json/" + badgeData["username"] + "/" + 
 		myRemovedFileList.appendChild(myList);
 		
 		var myModifiedFileList = document.createElement("div");
-		myModifiedFileList.innerHTML = "<span class=\"github-commit-badge-diffchanged\">Changed:<\/span>";
+		myModifiedFileList.innerHTML = "<span class=\"diffchanged\">Changed:<\/span>";
 		var myList = document.createElement("ul");
 		jQuery.each(modified, function(j, myModified) {
 			var myFile = document.createElement("li");
@@ -150,7 +149,7 @@ jQuery.getJSON("http://github.com/api/v1/json/" + badgeData["username"] + "/" + 
 		myBadge.appendChild(myFileList);
 
 		// and then the whole badge into the container
-		$("#github-commit-badge-container")[0].appendChild(myBadge);
+		$("#gcb-container")[0].appendChild(myBadge);
 
 		// initially hiding the file-list and the behaviour of the Show-files button
 		$("#" + myUser + myRepo).hide();	
@@ -163,7 +162,7 @@ jQuery.getJSON("http://github.com/api/v1/json/" + badgeData["username"] + "/" + 
 			};
 			return false;
 		});
-		$(".github-commit-badge-text-date").humane_dates();	// works here (still, ugly!)
+		$(".text-date").humane_dates();	// works here (still, ugly!)
 });
 });
 };
@@ -192,7 +191,7 @@ for (var i=0; i < myLibs.length; ++i) {
 		 //myScript.setAttribute("onload","mainpage();");
 		 document.getElementsByTagName("body")[0].setAttribute("onload","mainpage();");
 	};
-	document.getElementById("github-commit-badge-container").appendChild(myScript);
+	document.getElementById("gcb-container").appendChild(myScript);
 };
 
 // Write the stylesheet into the <head>
